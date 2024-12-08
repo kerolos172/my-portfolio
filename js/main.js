7ad4e0862466ef2e326499f1a8b1762bcb1feaf0
kerolos172 <kirolsmedhat2007@gmail.com>
2024-12-08 18:42:35 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// add class active to header on scroll
+let header = document.querySelector("header")
+window.onscroll = function(){
+    if(this.scrollY>=50){
+        header.classList.add("active")
+    }else{
+        header.classList.remove("active")
+    }
+}
